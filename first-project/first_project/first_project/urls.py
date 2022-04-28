8f719925cf5e0188ad737b703cc4555ff8cedbde
Gavrilov. E <Egor4it@yandex.ru>
2022-04-28 16:32:27 +0300

    //

--- first-project/first_project/first_project/urls.py
+++ first-project/first_project/first_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app import views
 from app.views import home_view
 
 
@@ -23,7 +24,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     # Раскомментируйте код, чтобы данные урлы 
     # обрабатывались Django
-    # path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
+    path('current_time/', views.time_view, name='time'),
+    path('workdir/', views.workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
 ]
